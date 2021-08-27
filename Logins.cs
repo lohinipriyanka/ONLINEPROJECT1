eaea1560716cd27ae3927f77d01963579bbf5aed
lohinipriyanka <89238725+lohinipriyanka@users.noreply.github.com>
2021-08-27 18:12:46 +0530

    Add files via upload

---
+++ Logins.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace onlineshopping.Models
+{
+    public class Logins
+    { 
+        [Required (AllowEmptyStrings =false, ErrorMessage ="UserID is Required")]
+        public int UserID { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "User_Name is Required")]
+        public int User_Name{ get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "User_Password is Required")]
+        public int User_Password { get; set; }
+    }
+}
