0f89b6411e10a3d7204145efcc1b4ac4fc15a8fe
lohinipriyanka <89238725+lohinipriyanka@users.noreply.github.com>
2021-08-27 12:51:48 +0530

    Add files via upload

---
+++ Main1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BAL;
+using DAL;
+using onlineshopping.Models;
+
+namespace onlineshopping.Controllers
+{
+    public class Main1Controller : Controller
+    {
+        ProductDal dal = new ProductDal();
+        // GET: Main1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult ShowProducts()
+        {
+            List<M_Products> p1 = new List<M_Products>();
+            List<Products> data = dal.GetProducts();
+            M_Products c = new M_Products();
+            foreach (var item in data)
+            {
+                
+                c.Product_ID = item.Product_ID;
+                c.Product_Name = item.Product_Name;
+                c.Specification = item.Specification;
+                c.Price = item.Price;
+                c.Product_image = item.Product_image;
+                p1.Add(c);
+
+            }
+            ViewBag.pname = c.Product_ID;
+             return View(p1);
+        }
+    }
+}
